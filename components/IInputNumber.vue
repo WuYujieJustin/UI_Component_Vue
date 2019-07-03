ace28922792bf4e196f1e59484d018d23bf128f5
WUYUJIE <ywu29@utas.edu.au>
2019-07-03 19:45:32 +0800

    formate input

--- components/IInputNumber.vue
+++ components/IInputNumber.vue
@@ -2,14 +2,13 @@
   <div class="IInputnumber">
     <div>
       <label for="IInputnumber" :style="{color:`${color}`}">{{label}}</label>
-      <input type="button" disabled v-if="suffixIcon" :value="`${unit}`"/>
-      <input 
+      <input type="button" disabled v-if="suffixIcon" :value="`${unit}`" />
+      <input
         ref="inputcompon"
         type="text"
         :placeholder="placeholder"
         :readonly="readonly"
         :suffixIcon="suffixIcon"
-        @keydown="hangdlekeydown"
         v-on="inputListeners"
       />
     </div>
@@ -17,6 +16,144 @@
 </template>
 
 <script>
+var NumeralFormatter = function(
+  numeralDecimalMark,
+  numeralIntegerScale,
+  numeralDecimalScale,
+  numeralThousandsGroupStyle,
+  numeralPositiveOnly,
+  stripLeadingZeroes,
+  prefix,
+  signBeforePrefix,
+  delimiter
+) {
+  var owner = this;
+
+  owner.numeralDecimalMark = numeralDecimalMark || ".";
+  owner.numeralIntegerScale = numeralIntegerScale > 0 ? numeralIntegerScale : 0;
+  owner.numeralDecimalScale =
+    numeralDecimalScale >= 0 ? numeralDecimalScale : 2;
+  owner.numeralThousandsGroupStyle =
+    numeralThousandsGroupStyle || NumeralFormatter.groupStyle.thousand;
+  owner.numeralPositiveOnly = !!numeralPositiveOnly;
+  owner.stripLeadingZeroes = stripLeadingZeroes !== false;
+  owner.prefix = prefix || prefix === "" ? prefix : "";
+  owner.signBeforePrefix = !!signBeforePrefix;
+  owner.delimiter = delimiter || delimiter === "" ? delimiter : ",";
+  owner.delimiterRE = delimiter ? new RegExp("\\" + delimiter, "g") : "";
+};
+
+NumeralFormatter.groupStyle = {
+  thousand: "thousand",
+  lakh: "lakh",
+  wan: "wan",
+  none: "none"
+};
+
+NumeralFormatter.prototype = {
+  getRawValue: function(value) {
+    return value
+      .replace(this.delimiterRE, "")
+      .replace(this.numeralDecimalMark, ".");
+  },
+
+  format: function(value) {
+    var owner = this,
+      parts,
+      partSign,
+      partSignAndPrefix,
+      partInteger,
+      partDecimal = "";
+
+    // strip alphabet letters
+    value = value
+      .replace(/[A-Za-z]/g, "")
+      // replace the first decimal mark with reserved placeholder
+      .replace(owner.numeralDecimalMark, "M")
+
+      // strip non numeric letters except minus and "M"
+      // this is to ensure prefix has been stripped
+      .replace(/[^\dM-]/g, "")
+
+      // replace the leading minus with reserved placeholder
+      .replace(/^\-/, "N")
+
+      // strip the other minus sign (if present)
+      .replace(/\-/g, "")
+
+      // replace the minus sign (if present)
+      .replace("N", owner.numeralPositiveOnly ? "" : "-")
+
+      // replace decimal mark
+      .replace("M", owner.numeralDecimalMark);
+
+    // strip any leading zeros
+    if (owner.stripLeadingZeroes) {
+      value = value.replace(/^(-)?0+(?=\d)/, "$1");
+    }
+
+    partSign = value.slice(0, 1) === "-" ? "-" : "";
+    if (typeof owner.prefix != "undefined") {
+      if (owner.signBeforePrefix) {
+        partSignAndPrefix = partSign + owner.prefix;
+      } else {
+        partSignAndPrefix = owner.prefix + partSign;
+      }
+    } else {
+      partSignAndPrefix = partSign;
+    }
+
+    partInteger = value;
+
+    if (value.indexOf(owner.numeralDecimalMark) >= 0) {
+      parts = value.split(owner.numeralDecimalMark);
+      partInteger = parts[0];
+      partDecimal =
+        owner.numeralDecimalMark + parts[1].slice(0, owner.numeralDecimalScale);
+    }
+
+    if (partSign === "-") {
+      partInteger = partInteger.slice(1);
+    }
+
+    if (owner.numeralIntegerScale > 0) {
+      partInteger = partInteger.slice(0, owner.numeralIntegerScale);
+    }
+
+    switch (owner.numeralThousandsGroupStyle) {
+      case NumeralFormatter.groupStyle.lakh:
+        partInteger = partInteger.replace(
+          /(\d)(?=(\d\d)+\d$)/g,
+          "$1" + owner.delimiter
+        );
+
+        break;
+
+      case NumeralFormatter.groupStyle.wan:
+        partInteger = partInteger.replace(
+          /(\d)(?=(\d{4})+$)/g,
+          "$1" + owner.delimiter
+        );
+
+        break;
+
+      case NumeralFormatter.groupStyle.thousand:
+        partInteger = partInteger.replace(
+          /(\d)(?=(\d{3})+$)/g,
+          "$1" + owner.delimiter
+        );
+
+        break;
+    }
+
+    return (
+      partSignAndPrefix +
+      partInteger.toString() +
+      (owner.numeralDecimalScale > 0 ? partDecimal.toString() : "")
+    );
+  }
+};
+
 export default {
   props: {
     // set for v-model
@@ -65,59 +202,101 @@ export default {
       type: Number,
       validator(val) {
         return val >= 0 && val === parseInt(val, 10);
-      }
+      },
+      default: 2
     }
   },
   mounted() {
-      this.tempvalue = this.value
-      console.log('created this.value', this.value)
-      this.$refs.inputcompon.value = this.value.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-  },
-  methods:{
-      hangdlekeydown(){
-        //   console.log(this.num)
-      }
-  },
-  watch: {
-    num: function() {
-      console.log(this.num);
-      this.num = this.num.replace(/,/g, "");
-      this.num = this.num.toString().replace(/(\d)(?=(?:\d{3})+$)/g, "$1,");
-      return this.num;
-    },
+    this.tempvalue = this.value;
+    (this.formatter = new NumeralFormatter(null, 0, 5))
+    //直接改变输入框内的值会改变光标的位置
+    this.$refs.inputcompon.value = this.value.toString()
+      .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
   },
   computed: {
-    xvalue() {
-        console.log('this.value', this.value)
-        let num = Number(this.value.toString().replace(',',''))
-        return num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-       
-    },
-    inputListeners: function () {
-         console.log('$listeners', Object.keys(this.$listeners))
-      var vm = this
+    inputListeners: function() {
+      var vm = this;
       // `Object.assign` 将所有的对象合并为一个新对象
-      return Object.assign({},
+      return Object.assign(
+        {},
         // 我们从父级添加所有的监听器
         this.$listeners,
         // 然后我们添加自定义监听器，
         // 或覆写一些监听器的行为
         {
-          // 这里确保组件配合 `v-model` 的工作
-          input: function (event) {
-            let str = vm.$refs.inputcompon.value
-            let num = Number(str.replace(/,/g, ''))
-            if(isNaN(num)) {
-                console.log('isNaN', num, str.replace(/,/g, ''))
-                num = vm.tempvalue
+          keyup(e){
+            vm.backspacePressed = e.keyCode === 8
+            vm.isMoveCursor = false
+             if (
+              vm.$refs.inputcompon.value[
+                vm.$refs.inputcompon.selectionStart-1
+              ] === ","
+            ) {
+              vm.isMoveCursor = true
+              console.log("move cursor")
             }
-            console.log('num is', num, 'str', str)
-            vm.tempvalue = num
-            vm.$refs.inputcompon.value = num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-            vm.$emit('input', num)
+          },
+          keydown(e) {
+            vm.backspacePressed = e.keyCode === 8
+            vm.oldstr = vm.$refs.inputcompon.value;
+            let position = vm.$refs.inputcompon.selectionStart
+          },
+          // 这里确保组件配合 `v-model` 的工作
+          input: function(event) {
+            let input = vm.$refs.inputcompon;
+            let rawStr = input.value;
+            if (rawStr === "") {
+              vm.$emit("input", 0);
+              vm.tempvalue = 0;
+              return;
+            }
+            let num = Number(rawStr.replace(/,/g, ""));
+            if (isNaN(num)) {
+              num = vm.tempvalue;
+            }
+            vm.tempvalue = num;
+
+            let str = vm.formatter.format(rawStr);
+            // .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+            // if(rawStr[rawStr.length-1] === '.') {
+            //   return
+            // }
+            if (str !== rawStr) {
+              let start = input.selectionStart;
+              let end = input.selectionEnd;
+              let rightStart = rawStr.length - start;
+              let rightEnd = rawStr.length - end;
+              // TODO： calculate start and end.
+              let newStart = str.length - rightStart;
+              let newEnd = str.length - rightEnd;
+              if (start === end) {
+                // if cursor on right of .
+                const pointIndex = str.indexOf(".");
+                if (pointIndex >= 0 && start > pointIndex) {
+                  newStart = start;
+                  newEnd = end;
+                }
+
+                if (vm.backspacePressed && vm.isMoveCursor) {
+                  // backspace pressed, move cursor before ','
+                  newStart--;
+                  newEnd--;
+                } else if (rawStr[start] === "," && rawStr[start - 1] === ",") {
+                  newStart++;
+                  newEnd++;
+                }
+                if (newStart < 0) {
+                  newStart = 0;
+                  newEnd = 0;
+                }
+              }
+              input.value = str;
+              input.setSelectionRange(newStart, newEnd);
+            }
+            vm.$emit("input", num);
           }
         }
-      )
+      );
     }
   }
 };
