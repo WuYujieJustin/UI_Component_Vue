68e69b5d6e4cb0d651d839e04972cbeac55051e0
WUYUJIE <ywu29@utas.edu.au>
2019-07-09 10:46:46 +0800

    delete some files

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,18 @@
 <template>
-  <div class="container">
-    <number-input class="red"></number-input>
-    <i-input-number/>
-  </div>
+
+    <time-select />
+
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import textInput from '~/components/textInput.vue'
-import numberInput from '~/components/numberInput.vue'
-import IInputNumber from '~/components/IInputNumber.vue'
+import numberInput from "~/components/numberInput.vue";
+import IInputNumber from "~/components/IInputNumber.vue";
+import timeSelect from "~/components/timeSelect.vue";
 export default {
   components: {
-    Logo,
     numberInput,
-    IInputNumber
+    IInputNumber,
+    timeSelect
   }
-}
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
